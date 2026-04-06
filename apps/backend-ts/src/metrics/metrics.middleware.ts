47ac28e86f4a6901c0bc54ff303df0a8b59a2124
Andre Tannus <andre.tannus@gmail.com>
2026-04-06 14:24:20 -0700

    Fixes loki-alloy config.

--- apps/backend-ts/src/metrics/metrics.middleware.ts
+++ apps/backend-ts/src/metrics/metrics.middleware.ts
@@ -1,18 +1,28 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import type { Request, Response, NextFunction } from 'express';
 import { MetricsService } from './metrics.service';
 
 @Injectable()
 export class MetricsMiddleware implements NestMiddleware {
+  private readonly logger = new Logger('HTTP');
+
   constructor(private readonly metrics: MetricsService) {}
 
   use(req: Request, res: Response, next: NextFunction) {
     const end = this.metrics.httpRequestDurationSeconds.startTimer();
     res.on('finish', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       const route = (req.route?.path as string | undefined) ?? req.path;
-      const labels = { method: req.method, route, status_code: String(res.statusCode) };
+      const labels = {
+        method: req.method,
+        route,
+        status_code: String(res.statusCode),
+      };
       this.metrics.httpRequestsTotal.inc(labels);
-      end({ method: req.method, route });
+      const duration = end({ method: req.method, route });
+      this.logger.log(
+        `${req.method} ${req.path} ${res.statusCode} ${(duration * 1000).toFixed(0)}ms`,
+      );
     });
     next();
   }
